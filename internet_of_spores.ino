10bb9c14ba796874ea0642c41f20ed4defeb1e47
Chris Baker <chris.l.baker@gmail.com>
2019-08-17 00:51:21 -0400

    Add support for SHT30 temperature and humidity sensor
    Lots of cleanup and reorganization of the file.
    Added overall framework for storing multiple types of sensor readings
    in the RTC memory.
    Added readout of SHT30 temp/humidity sensor using Wemos library.
    I previously had to modify this library but it is very simplistic so
    I will likely remove the dependency and implemet the I2C transactions
    directly.

--- internet_of_spores.ino
+++ internet_of_spores.ino
@@ -1,35 +1,75 @@
 #include <ESP8266WiFi.h>
+#include <WEMOS_SHT3X.h>
 #include <LOLIN_HP303B.h>
-#include <IOTAppStory.h>                // IotAppStory.com library
 
-#define SERIAL_SPEED 115200
+/* Configurations for IOT App Story */
+#define DEBUG_LVL         (2)
+#define SERIAL_SPEED      (115200)
+#define INC_CONFIG        (false)
+#define MAX_WIFI_RETRIES  (2)
+#define USEMDNS           (false)
+#include <IOTAppStory.h>
 
-// HP303B Opject
-LOLIN_HP303B HP303BPressureSensor;
+/* Global Configurations */
+#define COMPDATE          (__DATE__ __TIME__)
+#define MODEBUTTON        (0)                    // Button pin on the esp for selecting modes. D3 for the Wemos!
+#define SLEEP_TIME_US     (15000000)
+#define SLEEP_OVERHEAD_MS (120) //overhead guess (esp/stopwatch): 100=~.988, 200=1.008, 150=1.001 135=1.00082 130=1.00058 122=1.000078
+#define PREINIT_MAGIC     (0xAA559876)
+#define NUM_STORAGE_SLOTS (30)
 
-#define COMPDATE __DATE__ __TIME__
-#define MODEBUTTON 0                    // Button pin on the esp for selecting modes. D3 for the Wemos!
-IOTAppStory IAS(COMPDATE, MODEBUTTON);  // Initialize IotAppStory
+/* Types and Enums */
+// Macro to calculate the number of words that a
+// data structure will take up in RTC memory
+#define NUM_WORDS(x) (sizeof(x)/sizeof(uint32_t))
 
-//#define INCLUDE_TEMP
+// Structure to combine uptime with device flags
+typedef struct flags_time_s {
+  uint64_t flags    :8;
+  uint64_t reserved :16;
+  uint64_t millis   :40; //uptime in ms tracked over suspend cycles
+} flags_time_t;
 
-#define PREINIT_MAGIC 0xAA559876
-#define NUM_STORAGE_SLOTS 10
+// Labels for the different types of sensor readings
+typedef enum sensor_type_e {
+  SENSOR_UNKNOWN,
+  SENSOR_TEMPERATURE,
+  SENSOR_HUMIDITY,
+  SENSOR_PRESSURE,
+  SENSOR_PARTICLE,
+} sensor_type_t;
 
-typedef enum RTC_MEM {
-  RTC_MEM_CHECK         = 0,
-  RTC_MEM_BOOT_COUNT    = 1,
-  RTC_MEM_MILLIS        = 2,
-  RTC_MEM_FLAGS         = 3,
-  RTC_MEM_NUM_READINGS  = 4,
-  RTC_MEM_FIRST_READING = 5,
+// Structure to combine sensor readings with type and timestamp
+typedef struct sensor_reading_s {
+  sensor_type_t  type      :3;
+  uint64_t       timestamp :40;
+  int32_t        reading   :20;
+} sensor_reading_t;
+
+// Fields for each of the 32-bit fields in RTC Memory
+enum rtc_mem_fields_e {
+  RTC_MEM_CHECK = 0,
+  RTC_MEM_BOOT_COUNT,
+  RTC_MEM_FLAGS_TIME, //64-bit field
+  RTC_MEM_FLAGS_TIME_END = RTC_MEM_FLAGS_TIME + NUM_WORDS(flags_time_t) - 1,
+  RTC_MEM_NUM_READINGS,
+  RTC_MEM_FIRST_READING,
+
+  //array of sensor readings
+  RTC_MEM_DATA,
+  RTC_MEM_DATA_END = RTC_MEM_DATA + NUM_STORAGE_SLOTS*NUM_WORDS(sensor_reading_t) - 1,
 
   //keep last
-  RTC_MEM_DATA,
-  RTC_MEM_MAX = RTC_MEM_DATA + NUM_STORAGE_SLOTS,
-} RTC_MEM_T;
-uint32_t rtc_mem[RTC_MEM_MAX];
+  RTC_MEM_MAX
+};
 
+/* Global Data Structures */
+IOTAppStory IAS(COMPDATE, MODEBUTTON);
+SHT3X sht30(0x45);
+LOLIN_HP303B HP303BPressureSensor;
+uint32_t rtc_mem[RTC_MEM_MAX]; //array for accessing RTC Memory
+
+/* Functions */
 void preinit()
 {
   // Global WiFi constructors are not called yet
@@ -43,9 +83,10 @@ void preinit()
 uint64_t uptime(void)
 {
   uint64_t uptime_ms;
-  uptime_ms = rtc_mem[RTC_MEM_FLAGS] & 0xFF;
-  uptime_ms <<= 32;
-  uptime_ms += rtc_mem[RTC_MEM_MILLIS];
+  flags_time_t *timestruct = (flags_time_t*) &rtc_mem[RTC_MEM_FLAGS_TIME];
+
+  // boot timestamp + current millis()
+  uptime_ms = timestruct->millis;
   uptime_ms += millis();
   return uptime_ms;
 }
@@ -59,13 +100,18 @@ void serial_print_uptime(void)
   Serial.print(llu);
 }
 
-// helper to store pressure readings in the rtc mem ring buffer
-void store_reading(int32_t val)
+// helpers to store pressure readings in the rtc mem ring buffer
+void store_reading(sensor_type_t type, int32_t val)
 {
-  int slot = rtc_mem[RTC_MEM_FIRST_READING] + rtc_mem[RTC_MEM_NUM_READINGS];
+  sensor_reading_t *reading;
+  int slot;
+
+  // determine the next free slot in the RTC memory ring buffer
+  slot = rtc_mem[RTC_MEM_FIRST_READING] + rtc_mem[RTC_MEM_NUM_READINGS];
   if (slot >= NUM_STORAGE_SLOTS)
     slot -= NUM_STORAGE_SLOTS;
 
+  // update the ring buffer metadata
   if (rtc_mem[RTC_MEM_NUM_READINGS] == NUM_STORAGE_SLOTS)
     rtc_mem[RTC_MEM_FIRST_READING]++;
   else
@@ -74,42 +120,179 @@ void store_reading(int32_t val)
   if (rtc_mem[RTC_MEM_FIRST_READING] >= NUM_STORAGE_SLOTS)
     rtc_mem[RTC_MEM_FIRST_READING]=0;
 
-  //Serial.print("would store "); Serial.print(val); Serial.print(" @"); Serial.print(slot); Serial.print("("); Serial.print(RTC_MEM_DATA+slot); Serial.println(")");
-  rtc_mem[RTC_MEM_DATA+slot] = val;
+  // store the sensor reading in RTC memory at the given slot
+  reading = (sensor_reading_t*) &rtc_mem[RTC_MEM_DATA+slot*NUM_WORDS(sensor_reading_t)];
+  reading->type = type;
+  reading->reading = val;
+  reading->timestamp = uptime();
 }
 
 // helper to print the stored pressure readings from the rtc mem ring buffer
 void dump_readings(void)
 {
-  Serial.print("[");
+  const char typestrings[][11] = {
+    "UNKNOWN",
+    "TEMP (C)",
+    "HUMI (%)",
+    "PRES (kPa)",
+    "PART (kCt)",
+  };
+  char formatted[47];
+  formatted[46]=0;
 
-  for (unsigned long i=0; i < rtc_mem[RTC_MEM_NUM_READINGS]; i++)
-  {
-    int slot = rtc_mem[RTC_MEM_FIRST_READING] + i;
+  Serial.println("slot |     timestamp | type       |    value");
+  Serial.println("-----+---------------+------------+---------");
+
+
+  for (unsigned i=0; i < rtc_mem[RTC_MEM_NUM_READINGS]; i++) {
+    sensor_reading_t *reading;
+    const char* type;
+    int slot;
+
+    // find the slot indexed into the ring buffer
+    slot = rtc_mem[RTC_MEM_FIRST_READING] + i;
     if (slot >= NUM_STORAGE_SLOTS)
       slot -= NUM_STORAGE_SLOTS;
 
-    if (i != 0)
-      Serial.print(",");
-    Serial.print((int32_t)rtc_mem[RTC_MEM_DATA+slot]);
-  }
+    reading = (sensor_reading_t*) &rtc_mem[RTC_MEM_DATA+slot*NUM_WORDS(sensor_reading_t)];
 
-  Serial.println("]");
+    // determine the sensor type
+    switch(reading->type) {
+      case SENSOR_TEMPERATURE:
+        type=typestrings[1];
+      break;
+
+      case SENSOR_HUMIDITY:
+        type=typestrings[2];
+      break;
+
+      case SENSOR_PRESSURE:
+        type=typestrings[3];
+      break;
+
+      case SENSOR_PARTICLE:
+        type=typestrings[4];
+      break;
+
+      case SENSOR_UNKNOWN:
+      default:
+        type=typestrings[0];
+      break;
+    }
+
+    // format an output row with the data, type, and timestamp
+    snprintf(formatted, 45, "%4u | %13llu | %-10s | %+8.3f", i, reading->timestamp, type, reading->reading/1000.0);
+    Serial.println(formatted);
+  }
 }
 
 // helper for saving rtc memory before entering deep sleep
 void deep_sleep(uint64_t time_us)
 {
-  uint64_t uptime_ms = uptime() + time_us/1000 + 120; //overhead guess (esp/stopwatch): 100=~.988, 200=1.008, 150=1.001 135=1.00082 130=1.00058 122=1.000078
-  rtc_mem[RTC_MEM_MILLIS] = uptime_ms & 0xFFFFFFFF;
-  rtc_mem[RTC_MEM_FLAGS] = (uptime_ms  >> 32) & 0xFF;
+  flags_time_t *timestruct = (flags_time_t*) &rtc_mem[RTC_MEM_FLAGS_TIME];
+
+  // update the stored millis including some overhead for the write, suspend, and wake
+  timestruct->millis += millis() + time_us/1000 + SLEEP_OVERHEAD_MS;
+
+  // update the header checksum
   rtc_mem[RTC_MEM_CHECK] = PREINIT_MAGIC - rtc_mem[RTC_MEM_BOOT_COUNT];
+
+  // store the array to RTC memory and enter suspend
   ESP.rtcUserMemoryWrite(0, rtc_mem, sizeof(rtc_mem));
   ESP.deepSleep(time_us, RF_DISABLED);
 }
 
+// helper to read and store values from the SHT30 temperature and humidity sensor
+bool read_sht30(void)
+{
+  int ret;
+  ret = sht30.get();
+  Serial.println();
+  if (ret != 0) {
+    Serial.print("Error Reading SHT30 ret=");
+    Serial.println(ret);
+    return false;
+  } else {
+    store_reading(SENSOR_TEMPERATURE, sht30.cTemp*1000.0 + 0.5);
+    store_reading(SENSOR_HUMIDITY, sht30.humidity*1000.0 + 0.5);
+    Serial.print("Temperature: ");
+    Serial.print(sht30.cTemp, 3);
+    Serial.print("°C (");
+    Serial.print(sht30.fTemp, 3);
+    Serial.println("°F)");
+    Serial.print("Humidity: ");
+    Serial.println(sht30.humidity, 3);
+  }
+
+  return true;
+}
+
+// helper to read and store values from the HP303B barametric pressure sensor
+void read_hp303b(bool measure_temp)
+{
+  //OVERSAMPLING TABLE
+  // Val | Time (ms) | Noise (counts)
+  //   0 |      15.6 | 12
+  //   1 |      17.6 | 6
+  //   2 |      20.6 | 4
+  //   3 |      25.5 | 3
+  //   4 |      39.6 | 2
+  //   5 |      65.6 | 2
+  //   6 |     116.6 | 2
+  //   7 |     218.7 | 1
+  int16_t oversampling = 4;
+  int16_t ret;
+  int32_t pressure;
+
+
+  if (measure_temp) {
+    int32_t temperature;
+    //lets the HP303B perform a Single temperature measurement with the last (or standard) configuration
+    //The result will be written to the paramerter temperature
+    //ret = HP303BPressureSensor.measureTempOnce(temperature);
+    //the commented line below does exactly the same as the one above, but you can also config the precision
+    //oversampling can be a value from 0 to 7
+    //the HP303B will perform 2^oversampling internal temperature measurements and combine them to one result with higher precision
+    //measurements with higher precision take more time, consult datasheet for more information
+    ret = HP303BPressureSensor.measureTempOnce(temperature, oversampling);
+
+    if (ret != 0) {
+      //Something went wrong.
+      //Look at the library code for more information about return codes
+      Serial.print("Error Reading HP303B (temperature) ret=");
+      Serial.println(ret);
+    } else {
+      store_reading(SENSOR_TEMPERATURE, temperature*1000);
+      Serial.print("Temperature: ");
+      Serial.print(temperature);
+      Serial.print(" °C (");
+      Serial.print(((float)temperature * 9/5)+32.0);
+      Serial.println(" °F)");
+    }
+  }
+
+  //Pressure measurement behaves like temperature measurement
+  //ret = HP303BPressureSensor.measurePressureOnce(pressure);
+  ret = HP303BPressureSensor.measurePressureOnce(pressure, oversampling);
+  if (ret != 0) {
+    //Something went wrong.
+    //Look at the library code for more information about return codes
+    Serial.print("Error Reading HP303B (pressure) ret=");
+    Serial.println(ret);
+  } else {
+    store_reading(SENSOR_PRESSURE, pressure);
+    Serial.print("Pressure: ");
+    Serial.print(pressure/1000.0, 3);
+    Serial.print(" kPa (");
+    Serial.print((float)pressure/3386.39);
+    Serial.println(" in Hg)");
+  }
+}
+
 void setup(void)
 {
+  bool read_ok;
+
   Serial.begin(SERIAL_SPEED);
   delay(2);
   Serial.println();
@@ -123,93 +306,31 @@ void setup(void)
   Serial.print("setup: boot count=");
   Serial.print(rtc_mem[RTC_MEM_BOOT_COUNT]);
   Serial.print(", num readings=");
-  Serial.print(rtc_mem[RTC_MEM_NUM_READINGS]);
-  Serial.print(", first reading=");
-  Serial.println(rtc_mem[RTC_MEM_FIRST_READING]);
+  Serial.println(rtc_mem[RTC_MEM_NUM_READINGS]);
+  if (RTC_MEM_MAX > 128)
+    Serial.println("*************************\nWARNING RTC_MEM_MAX > 128\n*************************");
 
+  IAS.preSetDeviceName("spores");
   //IAS.begin('P');
 
-  //Call begin to initialize HP303BPressureSensor
-  //The parameter 0x76 is the bus address. The default address is 0x77 and does not need to be given.
-  //HP303BPressureSensor.begin(Wire, 0x76);
-  //Use the commented line below instead of the one above to use the default I2C address.
-  //if you are using the Pressure 3 click Board, you need 0x76
+  // Call begin to initialize HP303BPressureSensor
+  // The default address is 0x77 and does not need to be given.
   HP303BPressureSensor.begin();
+
+  //read temp/humidity from SHT30
+  read_ok = read_sht30();
+
+  //read temp/humidity from HP303B
+  read_hp303b(!read_ok);
+
+  dump_readings();
+
+  Serial.print("["); serial_print_uptime(); Serial.println("]...");
+  deep_sleep(SLEEP_TIME_US);
 }
 
 void loop(void)
 {
-  int32_t pressure;
-  int16_t oversampling = 4;
-  //OVERSAMPLING TABLE
-  // Val | Time (ms) | Noise (counts)
-  //   0 |      15.6 | 12
-  //   1 |      17.6 | 6
-  //   2 |      20.6 | 4
-  //   3 |      25.5 | 3
-  //   4 |      39.6 | 2
-  //   5 |      65.6 | 2
-  //   6 |     116.6 | 2
-  //   7 |     218.7 | 1
-
-  int16_t ret;
-
-#ifdef INCLUDE_TEMP
-  int32_t temperature;
-
-  Serial.print("["); serial_print_uptime(); Serial.print("] ");
-
-  //lets the HP303B perform a Single temperature measurement with the last (or standard) configuration
-  //The result will be written to the paramerter temperature
-  //ret = HP303BPressureSensor.measureTempOnce(temperature);
-  //the commented line below does exactly the same as the one above, but you can also config the precision
-  //oversampling can be a value from 0 to 7
-  //the HP303B will perform 2^oversampling internal temperature measurements and combine them to one result with higher precision
-  //measurements with higher precision take more time, consult datasheet for more information
-  ret = HP303BPressureSensor.measureTempOnce(temperature, oversampling);
-
-  if (ret != 0)
-  {
-    //Something went wrong.
-    //Look at the library code for more information about return codes
-    Serial.print("FAIL! ret = ");
-    Serial.println(ret);
-  }
-  else
-  {
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.print(" °C (");
-    Serial.print(((float)temperature * 9/5)+32.0);
-    Serial.println(" °F)");
-  }
-#endif
-
-  Serial.print("["); serial_print_uptime(); Serial.print("] ");
-  //Pressure measurement behaves like temperature measurement
-  //ret = HP303BPressureSensor.measurePressureOnce(pressure);
-  ret = HP303BPressureSensor.measurePressureOnce(pressure, oversampling);
-  if (ret != 0)
-  {
-    //Something went wrong.
-    //Look at the library code for more information about return codes
-    Serial.print("FAIL! ret = ");
-    Serial.println(ret);
-  }
-  else
-  {
-    Serial.print("Pressure: ");
-    Serial.print(pressure);
-    Serial.print(" Pa (");
-    Serial.print((float)pressure/3386.39);
-    Serial.println(" in Hg)");
-    store_reading(pressure);
-  }
-
-  dump_readings();
-
-
-  //Wait some time
-  Serial.print("["); serial_print_uptime(); Serial.println("]");
-  deep_sleep(15000000);
+  //should never get here
+  deep_sleep(SLEEP_TIME_US);
 }
