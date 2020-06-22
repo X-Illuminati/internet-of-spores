5806fe84f3f29ee16b320a63eb7dff7a1e6d35ba
Chris Baker <chris.l.baker@gmail.com>
2020-06-21 20:51:47 -0400

    flash.sh: add support for directly calling esptool

--- flash.sh
+++ flash.sh
@@ -2,7 +2,7 @@
 
 TOOLCHAIN_PATH="$HOME/.arduino15/packages/esp8266/hardware/esp8266"
 CHIP="esp8266"
-BAUD="921600"
+BAUD="460800"
 
 show_help()
 {
@@ -19,11 +19,25 @@ try_flash()
 	FILENAME="$2"
 	PORT="$3"
 
-	if [ ! -d "$tooldir" ]; then
+	if [ ! -d "$tooldir" -a ! "$tooldir" = "_esptool" ]; then
 		return 1
 	fi
 
 	case "$tooldir" in
+		_esptool)
+			if esptool.py version 2>/dev/null; then
+				echo "Using esptool.py from PATH to flash $FILENAME"
+				esptool.py --chip "$CHIP" --port "$PORT" --baud "$BAUD" --before default_reset --after hard_reset write_flash 0x0 "$FILENAME"
+			elif python3 -m esptool version 2>/dev/null; then
+				echo "Using python3 -m esptool to flash $FILENAME"
+				python3 -m esptool --chip "$CHIP" --port "$PORT" --baud "$BAUD" --before default_reset --after hard_reset write_flash 0x0 "$FILENAME"
+			elif python -m esptool version 2>/dev/null; then
+				echo "Using python -m esptool to flash $FILENAME"
+				python -m esptool --chip "$CHIP" --port "$PORT" --baud "$BAUD" --before default_reset --after hard_reset write_flash 0x0 "$FILENAME"
+			else
+				return 1
+			fi
+			;;
 		2.6.*)
 			if [ ! -f "$tooldir/tools/upload.py" ]; then
 				return 1
@@ -76,17 +90,21 @@ script_main()
 
 	if [ -d "$TOOLCHAIN_PATH" ]; then
 		cd "$TOOLCHAIN_PATH"
-		for toolchain in *; do
+		for toolchain in * _esptool; do
 			try_flash "$toolchain" "$FILENAME" "$PORT" && break
 		done
 		if [ $? -eq 0 ]; then
 			stty -F "$PORT" 115200 raw && cat "$PORT"
 			true
 		else
-			false
+			echo "Unable to flash:"
+			echo "  Either there is no valid toolchain at $TOOLCHAIN_PATH"
+			echo "  or the esptool python module could not be found"
+			echo "  or the esptool python module failed to flash the device at $PORT"
+			return 4
 		fi
 	else
-		echo "No toolchain found at $TOOLCHAIN_PATH"
+		echo "Invalid toolchain path: $TOOLCHAIN_PATH"
 		return 3
 	fi
 }
